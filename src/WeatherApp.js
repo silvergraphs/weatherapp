ec5683363a7ab756020c30c0cafcf79011653856
Bruno Caruso <brunofcaruso@gmail.com>
2020-09-30 17:03:03 -0300

    Maquetacion

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -7,17 +7,31 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="Weather-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style={{ marginTop: "0.3em", fontSize: "1.3em" }}>Weather App</h1>
+        <hr
+          color="white"
+          width="131vmin"
+          style={{ opacity: 0.3, marginBottom: "1.3em" }}
+        ></hr>
+        <strong style={{ fontSize: "2em" }}>23° C</strong>
+        <p style={{ fontSize: "0.8em", paddingBottom: "1em" }}>London, GA</p>
+        <div className="Weather-data">
+          <div style={{ float: "left" }}>
+            1009hPa
+            <p style={{ fontSize: "0.4em" }}>PRESSURE</p>
+          </div>
+          <div style={{ float: "left", paddingLeft: "0.85em" }}>
+            52%
+            <p style={{ fontSize: "0.4em" }}>HUMIDITY</p>
+          </div>
+          <div style={{ float: "left", paddingLeft: "0.85em" }}>
+            20° C<p style={{ fontSize: "0.4em" }}>MAX. TEMP</p>
+          </div>
+          <div style={{ float: "left", paddingLeft: "0.85em" }}>
+            -20° C<p style={{ fontSize: "0.4em" }}>MIN. TEMP</p>
+          </div>
+        </div>
+        [map]
       </header>
     </div>
   );
