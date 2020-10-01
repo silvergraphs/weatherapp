c6097b64f443c60c833b022bb79941f31a875d4b
Bruno Caruso <brunofcaruso@gmail.com>
2020-10-01 16:27:30 -0300

    Update README.md
    Se actualizo el README con una definicion correcta de la aplicacion

--- src/credentials.js
+++ src/credentials.js
@@ -1,4 +1,4 @@
 export default {
-  appId: "996cbb1d5e875cb5b95daac84a9f1f55", // OpenWeatherMap Token
+  appId: "996cbb1d5e875cb5b95daac84a9f1f55", // OpenWeatherMap API Key
   googleApiKey: "AIzaSyCPljZUvezU1eaM0zqEDcjdFDHujhSj03o", // Google JavaScript API Key
 };
